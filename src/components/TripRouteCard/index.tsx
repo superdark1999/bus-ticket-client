db84970d21fe4b261b16bcdc5c04f9de30a183c7
anhminh0401 <nguyenanhminh04012001@gmail.com>
2023-04-18 01:46:18 +0700

    add component TripRouteCard

---
+++ src/components/TripRouteCard/index.tsx
@@ -0,0 +1,157 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { Checkbox } from 'antd';
+import { EnvironmentOutlined, SwapRightOutlined, WifiOutlined, RestOutlined } from '@ant-design/icons';
+
+interface Props {
+  departure: string;
+  arrival: string;
+  price: number;
+  type: string;
+  seatsAvailable: number;
+  distance: string;
+  amenities: string[];
+  selected: boolean;
+  onSelect: () => void;
+}
+
+const onSelected = () => {};
+
+const temp: Props = {
+  departure: '8:00',
+  arrival: '12:00',
+  price: 289000,
+  seatsAvailable: 21,
+  amenities: ['air conditioning', 'restroom'],
+  selected: false,
+  onSelect: onSelected,
+  type: 'Limousine',
+  distance: '200km',
+};
+
+const BookingInfo: React.FC = () => {
+  useEffect(() => {});
+  return (
+    <Container>
+      <FirstRow>
+        {temp.departure}
+        <SwapRightOutlined />
+        {temp.arrival}
+        <Amenities>
+          {temp.amenities.includes('air conditioning') && (
+            <WifiOutlined style={{ fontSize: '16px', marginRight: '4px' }} />
+          )}
+          {temp.amenities.includes('restroom') && <RestOutlined style={{ fontSize: '16px', marginRight: '4px' }} />}
+        </Amenities>
+      </FirstRow>
+      <SecondRow>
+        {temp.price}đ
+        <Dot />
+        {temp.type}
+        <Dot />
+        {temp.seatsAvailable} chỗ
+      </SecondRow>
+      <RouteContainer>
+        <RouterInfo>
+          <RouteLine>
+            <LocationIcon />
+            Bến xe Phía Nam Nha Trang
+            <Distance>Xe tuyến: {temp.distance} = 9 tiếng</Distance>
+          </RouteLine>
+          <RouteLine>
+            <LocationIcon />
+            Bến xe Miền Tây
+          </RouteLine>
+        </RouterInfo>
+        <CheckboxContainer>
+          <Checkbox checked={temp.selected} onChange={temp.onSelect} />
+          Chọn
+        </CheckboxContainer>
+      </RouteContainer>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  padding: 20px;
+  border-radius: 8px;
+  border: 1px solid #dde2e8;
+  margin-bottom: 20px;
+`;
+
+const FirstRow = styled.div`
+  font-size: 24px;
+`;
+
+const Amenities = styled.div`
+  float: right;
+`;
+
+const SecondRow = styled.div`
+  color: #111;
+  margin: 8px 0 16px;
+  font-size: 15px;
+  min-width: 200px;
+  height: 28px;
+  border-radius: 16px;
+  padding: 5px 12px;
+  display: inline-block;
+  background-color: rgba(99, 114, 128, 0.1);
+`;
+
+const Dot = styled.span`
+  display: inline-block;
+  width: 6px;
+  border-radius: 3px;
+  height: 6px;
+  opacity: 0.3;
+  margin: 3px 12px;
+  background-color: #000;
+`;
+
+const RouteContainer = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const RouterInfo = styled.div`
+  flex: 1;
+`;
+
+const RouteLine = styled.div`
+  color: #111;
+  font-size: 15px;
+  position: relative;
+  border-left: 2px dotted #c0c6cc;
+  padding-left: 22px;
+  margin-left: 8px;
+
+  &:last-child {
+    border-left: 2px dotted #fff;
+  }
+`;
+
+const LocationIcon = styled(EnvironmentOutlined)`
+  width: 16px;
+  top: 0;
+  left: -9px;
+  position: absolute;
+`;
+
+const Distance = styled.div`
+  font-size: 13px;
+  color: #00613d;
+  line-height: 48px;
+`;
+
+const CheckboxContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  color: #01613d;
+  font-size: 13px;
+  width: 34px;
+`;
+
+export default BookingInfo;
---
+++ src/views/Booking/listTripRoute.tsx
@@ -0,0 +1,14 @@
+import BookingInfo from 'components/TripRouteCard';
+import React from 'react';
+
+const ListTripRoute: React.FC = () => {
+  const content = 'hihi';
+  return (
+    <div>
+      <BookingInfo />
+      {content}
+    </div>
+  );
+};
+
+export default ListTripRoute;
