a03c6afb2d6282c9d7634aef5960fbb61abe2003
Anh Khoa <phunganhkhoa123@gmail.com>
2023-05-19 21:15:04 +0700

    resolve conflic

--- src/layout/BookingLayout.tsx
+++ src/layout/BookingLayout.tsx
@@ -17,11 +17,10 @@ export default function BookingLayout() {
   );
 }
 
+// cái height làm cho màn hình ko kéo dc nên Minh đã chuyển overflow từ hidden thành auto nhưng làm vậy sẽ ảnh hưởng 
+// tơí cái header của em không nổi lên dc 
 const FullScreenStyled = styled.div`
   width: 100vw;
-
-  // cái height làm cho màn hình ko kéo dc nên Minh đã chuyển overflow từ hidden thành auto nhưng làm vậy sẽ ảnh hưởng 
-  // tơí cái header của em không nổi lên dc 
   height: 100vh;  
   overflow: auto;
 
