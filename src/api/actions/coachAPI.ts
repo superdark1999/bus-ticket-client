50981c32fd2ff8e04dada1af5334724d0cbb4fdb
Nguyen Van Loi <nguyenvanloi10101@gmail.com>
2023-06-09 00:43:33 +0700

    delete coach, update coach

--- src/api/actions/coachAPI.ts
+++ src/api/actions/coachAPI.ts
@@ -18,7 +18,22 @@ const createNewCoach = async (model: any, capacity: any, registrationNumber: any
     return newCoach;
 }
 
+const deleteCoach = async (id:any) => {
+    const url = `/coach/delete/${id}`;
+    await axiosAdmin.axiosAdminCoach.delete(url);
+}
+
+const updateCoach = async (id: any, capacity:any, model:any, registrationNumber:any) => {
+    const url = '/coach/update';
+    const res = await axiosAdmin.axiosAdminCoach.patch(url, {id, capacity, model, registrationNumber});
+
+    const {newCoach} = res.data;
+    return newCoach;
+}
+
 export default {
     getCoachList,
-    createNewCoach
+    createNewCoach,
+    deleteCoach,
+    updateCoach,
 }
