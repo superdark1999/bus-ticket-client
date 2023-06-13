22a09ef538eec958bc1d1011abcb5065d1b670b3
ntdtiendat712 <dat.nguyen@devsamurai.com>
2023-06-13 22:49:41 +0700

    fix trip route url

--- src/api/utils/constant.ts
+++ src/api/utils/constant.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = {
   adminTrip: 'http://www.busticket.net.eu.org/trip', // resful API
-  adminTripRoute: 'http://www.busticket.net.eu.org',
+  adminTripRoute: 'http://www.busticket.net.eu.org/trip-route',
   adminCoach: 'http://www.busticket.net.eu.org',
   user: '',
   booking: '',
